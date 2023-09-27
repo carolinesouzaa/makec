a734f537d60fff4aacc8bb8053778cc7e6faf307
carolinesouzaa <carolinevmsouza@gmail.com>
2023-09-27 08:39:55 -0300

    Aula do dia 22-09.
    Componentização.

---
+++ src/pages/Sacola/componentes/Topo.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Image, Text, StyleSheet, Dimensions } from 'react-native';
+
+import topo from "../../../../assets/topo.png";
+
+const widht = Dimensions.get("screen").width;
+
+export default function Topo() {
+    return <>
+        <Image source={topo} style={estilos.topo} />
+        <Text style={estilos.titulo}>Detalhe do Carrinho</Text>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: (578 / 768) * widht,
+    },
+        titulo: {
+        fontSize: 24,
+        fontWeight: "bold",
+        lineHeight: 26,
+        width: "100%",
+        textAlign: "center",
+        color: "white",
+        position: "absolute",
+        padding: 16,
+    }
+});
